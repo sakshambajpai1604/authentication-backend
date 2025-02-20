5cbe139e6805a3da2b9874bad7246f635b6f3c43
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-02-20 22:20:49 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://sakshambajpai:1234@cluster0.p5yvn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb://localhost:27017/express-auth');
 
 const userSchema = mongoose.Schema({
     username: String,
